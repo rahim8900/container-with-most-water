ee9317eed321cff98ae0ec844f234b16161f856f
rahim8900 <rehimhesenov26@gmail.com>
2024-05-08 15:33:38 +0400

    Add project files.

---
+++ Container-with-most-water/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Container_with_most_water
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(',');
+            int max = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if(i != j)
+                    {
+                         int area = Math.Min(int.Parse(input[i]), int.Parse(input[j])) * Math.Abs(i-j);
+                        if(area > max)
+                        {
+                            max = area;
+                        }
+                    }                    
+                }                
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
